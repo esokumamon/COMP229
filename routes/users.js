450e24a053d160ac0c81f8982c28bf33249dd205
liangyu wang <lwang94@my.centennialcollege.ca>
2022-01-27 00:43:22 -0500

    first version of portfolio page, finished basic layout of website

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+// Liangyu Wang 
+// 980025288
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
